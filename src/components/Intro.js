a3457b99c939c706ec0cd1e1f01303d0291f2689
John Vladimir Monge <87348858+VladNotDev@users.noreply.github.com>
2022-06-17 06:02:35 +0800

    a

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ import React from "react"
 export default function Intro(props){
     return (
         <div className="intro"> 
-            <h1> Quizzical </h1>
+            <h1> Quizzica </h1>
             <p> Some descriptions if needed </p>
             <button onClick={props.clicked}> Start Quiz </button>
         </div>
