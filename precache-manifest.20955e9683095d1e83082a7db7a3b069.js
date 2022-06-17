8f63a79ebcadceff51af4ae134d5bd4d41c55b0a
John Vladimir Monge <87348858+VladNotDev@users.noreply.github.com>
2022-06-17 09:34:09 +0800

    Updates

---
+++ precache-manifest.20955e9683095d1e83082a7db7a3b069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a01dc997b7dcceedd9",
+    "url": "/Quizzica/static/css/main.30953760.chunk.css"
+  },
+  {
+    "revision": "91a01dc997b7dcceedd9",
+    "url": "/Quizzica/static/js/main.ff11f236.chunk.js"
+  },
+  {
+    "revision": "e090af5c0ace5e98ddcb",
+    "url": "/Quizzica/static/js/runtime~main.a4cb9567.js"
+  },
+  {
+    "revision": "f5b962e602cae2c58ddc",
+    "url": "/Quizzica/static/js/2.a93ff080.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/Quizzica/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "b531cefee405c3bd15874d0c77afbe12",
+    "url": "/Quizzica/index.html"
+  }
+];
